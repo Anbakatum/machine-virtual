41b2e29fb9255d57de5a8cd32c19056aec029e2e
Anbakatum <rhuanggezryan@gmail.com>
2026-09-09 15:56:54 -0300

    Update installation script for Ubuntu 24.04
    Removed 'libegl1-mesa' from the installation command for Ubuntu 24.04.

--- 03_install_sunshine.sh
+++ 03_install_sunshine.sh
@@ -5,8 +5,10 @@ set -e
 echo "=== 3/5. Instalando XFCE4 e Sunshine ==="
 sudo echo 'debconf debconf/frontend select Noninteractive' | sudo debconf-set-selections
 sudo apt-get update -y
+
+# Pacotes corrigidos para o Ubuntu 24.04 (Noble)
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" \
-    xvfb x11vnc xfce4 xfce4-terminal libgbm1 libegl1-mesa wget curl libssl-dev
+    xvfb x11vnc xfce4 xfce4-terminal libgbm1 libegl1 wget curl libssl-dev
 
 if ! command -v sunshine &> /dev/null; then
     wget -q https://github.com/LizardByte/Sunshine/releases/latest/download/sunshine-ubuntu-22.04-amd64.deb -O /tmp/sunshine.deb
