5e2593a1fda25f753ca468c93939249cd9028ecc
Anbakatum <rhuanggezryan@gmail.com>
2026-09-09 16:15:38 -0300

    Delete 02_install_tailscale.sh

--- 02_install_tailscale.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-export DEBIAN_FRONTEND=noninteractive
-set -e
-
-echo "=== 2/5. Instalando Tailscale ==="
-if ! command -v tailscale &> /dev/null; then
-    curl -fsSL https://tailscale.com/install.sh | sh
-fi
