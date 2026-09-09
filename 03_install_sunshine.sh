90c62828747d4cfc0f62ff7c335bcd9409ff3ecb
Anbakatum <rhuanggezryan@gmail.com>
2026-09-09 16:06:22 -0300

    Update 03_install_sunshine.sh

--- 03_install_sunshine.sh
+++ 03_install_sunshine.sh
@@ -6,12 +6,12 @@ echo "=== 3/5. Instalando XFCE4 e Sunshine ==="
 sudo echo 'debconf debconf/frontend select Noninteractive' | sudo debconf-set-selections
 sudo apt-get update -y || true
 
-# Instalação dos componentes gráficos base
+# Componentes gráficos base
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" \
     xvfb x11vnc xfce4 xfce4-terminal libgbm1 libegl1 wget curl libssl-dev libboost-program-options-dev libminiupnpc-dev libevdev-dev
 
-# Instalar Sunshine AppImage portátil e registrar no sistema
-if ! command -v sunshine &> /dev/null; then
-    wget -q https://github.com/LizardByte/Sunshine/releases/latest/download/sunshine.AppImage -O /usr/bin/sunshine
-    chmod +x /usr/bin/sunshine
+# Baixa AppImage do Sunshine e concede permissões
+if [ ! -f /usr/bin/sunshine ]; then
+    sudo wget -q https://github.com/LizardByte/Sunshine/releases/latest/download/sunshine.AppImage -O /usr/bin/sunshine
+    sudo chmod 755 /usr/bin/sunshine
 fi
