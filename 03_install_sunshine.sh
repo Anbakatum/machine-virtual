769b65cfceab74267a81889add627fb8840ce39d
Anbakatum <rhuanggezryan@gmail.com>
2026-09-09 15:59:09 -0300

    Update 03_install_sunshine.sh

--- 03_install_sunshine.sh
+++ 03_install_sunshine.sh
@@ -4,14 +4,16 @@ set -e
 
 echo "=== 3/5. Instalando XFCE4 e Sunshine ==="
 sudo echo 'debconf debconf/frontend select Noninteractive' | sudo debconf-set-selections
-sudo apt-get update -y
+sudo apt-get update -y || true
 
-# Pacotes corrigidos para o Ubuntu 24.04 (Noble)
+# Instalacao dos componentes graficos base
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" \
     xvfb x11vnc xfce4 xfce4-terminal libgbm1 libegl1 wget curl libssl-dev
 
+# Instalar Sunshine
 if ! command -v sunshine &> /dev/null; then
     wget -q https://github.com/LizardByte/Sunshine/releases/latest/download/sunshine-ubuntu-22.04-amd64.deb -O /tmp/sunshine.deb
-    sudo DEBIAN_FRONTEND=noninteractive dpkg -i /tmp/sunshine.deb || sudo DEBIAN_FRONTEND=noninteractive apt-get install -fy
+    sudo dpkg -i /tmp/sunshine.deb || true
+    sudo DEBIAN_FRONTEND=noninteractive apt-get install -f -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold"
     rm -f /tmp/sunshine.deb
 fi
